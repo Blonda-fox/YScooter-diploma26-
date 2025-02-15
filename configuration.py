d1013ef96a7c180e324dec8be8c19293f76f3dac
Blonda-fox <poedinceva95@gmail.com>
2025-02-15 22:41:15 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://b18a9d33-8f88-4872-a046-7a5babbb60f6.serverhub.praktikum-services.ru"
+CREATE_ORDERS = "/api/v1/orders"
+FETCH_ORDER_TRACK = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+        "firstName": "Дария",
+        "lastName": "Красотка",
+        "address": "СПБточкаРУ",
+        "metroStation": 18,
+        "phone": "+78002000700",
+        "rentTime": 3,
+        "deliveryDate": "2025-02-09",
+        "comment": "Сделайте красиво",
+        "color": ["BLACK"]
+    }
